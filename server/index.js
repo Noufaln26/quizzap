472ed1be285f47d26ae3f566e153ba0cc9fbd9f4
Noufal N <noufaln26@gmail.com>
2026-02-28 14:56:49 +0530

    Update CLIENT_URL

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ const CLIENT_URL = process.env.CLIENT_URL || 'http://localhost:5173';
 
 const io = new Server(server, {
   cors: {
-    origin: CLIENT_URL,
+    origin: *,
     methods: ['GET', 'POST'],
   },
 });
